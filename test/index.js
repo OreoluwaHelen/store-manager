7c63c9f5287f8b1398f7def5479f07ca8a00a901
oreoluwahelen <adenugahelen94@gmail.com>
2018-10-19 19:52:47 -0700

    (feat) : add product
    -add products to a data structure.
    
    [(Finishes)] #161296535

--- test/index.js
+++ test/index.js
@@ -5,6 +5,13 @@ import app from '../server/index';
 
 const request = supertest(app);
 const { expect } = chai;
+const product = {
+  id: 2,
+  name: 'macbook',
+  desc: 'good laptop',
+  stock: 2,
+  availableStock: 1,
+};
 
 describe('initial Testing', () => {
   it('should display welcome', (done) => {
@@ -13,6 +20,26 @@ describe('initial Testing', () => {
       .end((err, res) => {
         expect(res.body.message).to.equal('welcome to your store manager');
         done();
+      });
+  });
+});
+
+describe('Product Endpoint', () => {
+  it('should create ', (done) => {
+    request
+      .post('/api/v1/products')
+      .send({
+        name: 'macbook',
+        desc: 'good laptop',
+        stock: 2,
+        availableStock: 1,
+      })
+      .end((err, res) => {
+        expect(res.body.status).to.equal('success');
+        expect(res.body.message).to.equal('product was created successfully');
+        expect(res.body.product).to.be.an('object');
+        expect(res.body.product.id).to.equal(product.id);
+        done(err);
       });
   });
 });
