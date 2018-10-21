53a57de90249ba1a2cf047616edf447bd5dfc389
oreoluwahelen <adenugahelen94@gmail.com>
2018-10-20 18:05:44 -0700

    (feat): get a sale order
    -get a single sale order with id
    
    |(Finishes) #161363196]

--- server/controller/salesController.js
+++ server/controller/salesController.js
@@ -3,6 +3,7 @@ import db from '../db';
 const { sales } = db;
 
 const salesController = {
+
   createSale: (req, res) => {
     const order = {
       id: sales.length + 1,
@@ -17,6 +18,29 @@ const salesController = {
       order,
     });
   },
+
+
+  getSingleSaleOrder: (req, res) => {
+    let order = null;
+    sales.forEach((o) => {
+      if (o.id === Number(req.params.orderId)) {
+        order = o;
+      }
+    });
+
+    if (order === null) {
+      return res.status(404).send({
+        status: 'fail',
+        message: 'order not found',
+      });
+    }
+    return res.status(200).send({
+      status: 'success',
+      order,
+
+    });
+  },
+
 };
 
 export default salesController;
