8edc16ac8f418743c247148626cfb65b3bcc9082
oreoluwahelen <adenugahelen94@gmail.com>
2018-11-01 18:24:13 -0700

    fixing

--- server/config.js
+++ server/config.js
@@ -2,13 +2,26 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-let connectionString = ();
+let connectionString = '';
+
+if ( process.env.NODE_ENV === 'test') {
+  connectionString = process.env.DATABASE_TEST;
+} else if (process.env.NODE_ENV === 'development') {
+  connectionString = process.env.DATABASE_DEV;
+} else {
+  connectionString = process.env.DATABASE_URL;
+}
+
 
 const pool = pg.pool({ connectionString });
 
 export default (callback) => {
   pool.connect ((err, client,done) => callback(err, client, done));
 };
+
+
+
+
 
 // // const connectionString = process.env.DARTABASE_URLORE;
 // // const connectionString = 'postgres://localhost:5432/store_manager';
@@ -21,3 +34,4 @@ export default (callback) => {
 // });
 
 // export default client;
+
